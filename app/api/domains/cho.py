7720bedbe7c5cde53f2107332cbce63f55135514
minisbett <39670899+minisbett@users.noreply.github.com>
2024-02-04 19:52:10 +0100

    Remove country hotfix for testing

--- app/api/domains/cho.py
+++ app/api/domains/cho.py
@@ -779,7 +779,7 @@ async def login(
             ),
         }
 
-    if True:
+    if db_country == "xx":
         # bugfix for old bancho.py versions when
         # country wasn't stored on registration.
         log(f"Fixing {login_data['username']}'s country.", Ansi.LGREEN)
